c9fd9c4451e01bbbfcb7f8d9c6a96fcf0e8a5cf7
webmaster <webmaster@L05MAC05.local>
2013-02-21 22:01:30 +0100

    first commit

---
+++ htdocs/zf_shop/application/controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+class ProductController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function wijzigenAction()
+    {
+        // action body
+    }
+
+    public function toevoegenAction()
+    {
+        // action body
+    }
+
+    public function verwijderenAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
